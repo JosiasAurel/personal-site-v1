d9de57ba7b1ddf12488efa0f6e224ffad025a0ba
JosiasAurel <ndjosiasaurel@gmail.com>
2020-11-01 11:18:21 +0100

    add social icons on post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -30,6 +30,13 @@ export default function PostTemplate({content, data}) {
 
           </Post>
 
+	 <div className={styles.footer}>
+			<a href="https://github.com/JosiasAurel"><img src="/github.png" alt="github" className={styles.social} /></a>		 
+			<a href="mailto:%20ndjosiasaurel@gmail.com"><img src="/gmail.png" alt="github" className={styles.social} /></a>		 
+			<a href="https://twitter.com/JosiasWing?s=09"><img src="/twitter.png" alt="github" className={styles.social} /></a>		 
+			<a href="https://josiasaurel.itch.io/"><img src="/itchio.png" alt="github" className={styles.social} /></a>		 
+			</div>
+
           <h2 className={styles.c}> © 2020 Josias Aurel </h2>
          </div>
         )
