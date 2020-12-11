27e4bc438f05002b99a11804f68b0df003b17c01
JosiasAurel <ndjosiasaurel@gmail.com>
2020-12-11 02:51:33 +0100

    make the header pink

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,24 @@
 import React from "react"
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark"
+import style from "../styles/index.module.css"
 // import styles
 import styles from "../styles/index.module.css"
 const Index = () => {
-    <div>
-        <header>
-            <a href="">Uses</a>
-            <a href="">Blog</a>
-            <a href="">Projects</a>
-            <a href="">About</a>
+    return (
+        <div>
+        <header className={styles.header}>
+            <span className={styles.headerLinks}>
+            <a className={styles.headerLink} href="">Uses</a>
+            <a className={styles.headerLink} href="">Blog</a>
+            <a className={styles.headerLink} href="">Projects</a>
+            <a className={styles.headerLink} href="">About</a>
+            </span>
         </header>
 
         <div className={style.typewriter}>
             <h1 className={styles.name}>Hi, I'm Josias Aurel</h1>
         </div>
     </div>
+    )
 }
 
 export default Index
