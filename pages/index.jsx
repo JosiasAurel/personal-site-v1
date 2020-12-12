401488924077dd752965f31c295d7e21fa7d8f92
JosiasAurel <ndjosiasaurel@gmail.com>
2020-12-12 14:45:33 +0100

    add the social image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark"
 import Header from "../components/header"
 // import styles
 import styles from "../styles/index.module.css"
@@ -13,12 +14,23 @@ const Index = () => {
         
         <div className={styles.present}>
             <div className={styles.typewriter}>
-            <h1 className={styles.name}>Hi, I'm Josias Aurel</h1>
+            <h1 className={styles.name}>Hey, I'm Josias Aurel</h1>
         </div>
         </div>
 
-        <div>
+        <div className={styles.message}>
             <p>It's nice to have you here.</p>
+            <p>I am a hobbyist self-taugh developer from Cameroon.
+                <br/>
+                I code everyday on random stuff or my projects, because coding it is fun for me.
+                <br/>
+                <p>You can reach to me on Twitter - that's where i usually hangout.</p>
+            </p>
+            <p>I am an occasional Open Source contributor and maintainer.
+                <br/>
+                Fun fact : I write bugs XD.
+            </p>
+           
         </div>
 
     </div>
