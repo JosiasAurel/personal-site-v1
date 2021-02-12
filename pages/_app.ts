67f6b3937885105d2f03073a5c5389cb154b6b73
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-12 18:50:52 +0100

    type all pages

---
+++ pages/_app.ts
@@ -0,0 +1,11 @@
+import "../styles/global.css"
+import React, { FunctionComponent } from "react"
+import { AppProps } from "next/app"
+
+const App: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
+  return (
+    <Component {...pageProps} />
+  )
+};
+
+export default App;
