9522747b66c76836731d71d1b7ef5fe3f80a6620
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-13 17:23:13 +0100

    add google analytics

--- pages/_document.ts
+++ pages/_document.ts
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class JADocument extends Document {
+
 	render() {
 		return (
 			<Html>
@@ -8,7 +9,9 @@ class JADocument extends Document {
 <meta name="monetization" content="$ilp.uphold.com/ZQJwqFyPPwrF" />
 				 <meta name="theme-color" content="black" />
                   <link rel="stylesheet" href="/style.css" />
-					<link rel="shortcut icon" href="/josias.jpg" />					    
+					<link rel="shortcut icon" href="/josias.jpg" />	
+					<script src="/gtag.js">
+					</script>				    
 				</Head>
 				<body>
 				<Main />
